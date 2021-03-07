0700e9277afdb590fb72de986c763cc9d4d9ef45
RaifIT <raif5it@gmail.com>
2021-03-07 14:25:54 +0300

    Added model

--- RazorPagesMovie/Startup.cs
+++ RazorPagesMovie/Startup.cs
@@ -8,21 +8,36 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RazorPagesMovie.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace RazorPagesMovie
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            Environment = env;
         }
+
+        public IWebHostEnvironment Environment { get; }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+             if (Environment.IsDevelopment()){
+                  services.AddDbContext<RazorPagesMovieContext>(options =>
+                  options.UseSqlite(
+                      Configuration.GetConnectionString("RazorPagesMovieContext")));
+             }
+             else{
+                 services.AddDbContext<RazorPagesMovieContext>(options =>
+                 options.UseSqlServer(
+                     Configuration.GetConnectionString("MovieContext")));
+             }
             services.AddRazorPages();
         }
 
